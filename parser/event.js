69dcb9303304f9a50456705fbf51515894f74553
Justin Liu <yuting1987@gmail.com>
2014-06-17 00:18:54 +0800

    debug

--- parser/event.js
+++ parser/event.js
@@ -8,11 +8,11 @@ try {
   var Release = DataBase.Release;
 
   var parser = function (cb){
-    var date = new Date().toISOString().replace(/T.*/gi, '');
+    var date = new Date().toISOString();
 
     var source = [
-      'https://www.googleapis.com/calendar/v3/calendars/s6jage479tquhj3mr7abhecs48%40group.calendar.google.com/events?key=AIzaSyBqSFbeQLYKQl80FblMuj682zvpbpPVG_o&timeZone=Asia/Taipei&timeMin=' + date + 'T00:00:00.000Z',
-      'https://www.googleapis.com/calendar/v3/calendars/ptt.publicissue%40gmail.com/events?key=AIzaSyBqSFbeQLYKQl80FblMuj682zvpbpPVG_o&timeZone=Asia/Taipei&timeMin=' + date + 'T00:00:00.000Z',
+      'https://www.googleapis.com/calendar/v3/calendars/s6jage479tquhj3mr7abhecs48%40group.calendar.google.com/events?key=AIzaSyBqSFbeQLYKQl80FblMuj682zvpbpPVG_o&timeZone=Asia/Taipei&timeMin='+date,
+      'https://www.googleapis.com/calendar/v3/calendars/ptt.publicissue%40gmail.com/events?key=AIzaSyBqSFbeQLYKQl80FblMuj682zvpbpPVG_o&timeZone=Asia/Taipei&timeMin='+date,
     ];
 
     async.map(source, function (url, cb) {
@@ -34,7 +34,9 @@ try {
 
       results.forEach(function (list) {
         list.forEach(function (item) {
-          var start = (item.start.date ? item.start.date + 'T00:00:00+08:00' : item.start.dateTime);
+          var start = 
+            item.start && (item.start.date && item.start.date + 'T00:00:00+08:00' || item.start.dateTime)
+            || new Date(new Date().getTime()+8*60*60*1000).toISOString().replace(/\..+/gi,'+08:00');
 
           if ( item.recurrence ) {
             var opt = RRule.parseString((item.recurrence+'').replace('RRULE:',''))
@@ -59,14 +61,16 @@ try {
           }
 
           eData['type'] = 'google';
-          eData['day'] = item.start.date ? true : false;
+          eData['day'] = item.start && item.start.date ? true : false;
           eData['start'] = start;
-          eData['end'] = (item.end.date ? item.end.date + 'T00:00:00+08:00' : item.end.dateTime) || '';
+          eData['end'] = item.end && (item.end.date && item.end.date + 'T00:00:00+08:00' || item.end.dateTime) || '';
           eData['title'] = item.summary || '';
           eData['location'] = item.location || '';
           eData['link'] = eData['link'] || item.htmlLink;
 
-          events['google_' + item.id] = eData;
+          if (item.summary) {
+            events['google_' + item.id] = eData;
+          }
 
           count += 1;
         });
